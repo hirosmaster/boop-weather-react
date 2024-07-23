022e1fe79c0e5005137813d9565363cf495112a7
Andrea <andrea.soriano@outlook.com>
2024-07-23 15:35:27 +0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Work from "./Work.js";
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>yo pls work</h1>
       <h2>ok pls continue to work</h2>
       <Work />
+      <div>
+        <p>
+          This projected was created by{" "}
+          <a href="https://github.com/hirosmaster">Rica Andrea Soriano</a> and
+          is open sourced on{" "}
+          <a href="https://github.com/hirosmaster/boop-weather-react">Github</a>{" "}
+          and is hosted on <a href="boopyweather.netlify.app">Netlify.</a>
+        </p>
+        <btn btn-primary>YO WASSUP</btn>
+      </div>
     </div>
   );
 }
